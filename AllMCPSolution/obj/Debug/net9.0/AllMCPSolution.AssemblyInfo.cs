9b9a25ebbdcc26b516d764ee475c3fc3990b83fe
johann <johann@bowlinedigital.com>
2025-10-12 14:10:34 +0200

    hä?

--- AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
+++ AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fe505f13d9ad4860afbb7335eee02a3d6dcc29")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
