1029b928c801b303bd0dcaa72c29f1b882548a78
johann <johann@bowlinedigital.com>
2025-10-17 20:50:37 +0200

    adds score to bottle

--- AllMCPSolution/Models/Bottle.cs
+++ AllMCPSolution/Models/Bottle.cs
@@ -4,6 +4,8 @@ public class Bottle
 {
     public Guid Id { get; set; }
     public string TastingNote { get; set; } = string.Empty;
+    public decimal? Price { get; set; }
+    public decimal? Score { get; set; }
 
     public Guid WineId { get; set; }
     public Wine Wine { get; set; } = null!;
