34e985cf6781ba00a3ab4cdb6d2a5ff790ff7a47
johann <johann@bowlinedigital.com>
2025-10-19 10:44:45 +0200

    i hope this works

--- AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
+++ AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db80488c5aeec87257f41c68ca279c3cd76fe00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e763b7e9ea3da8b655231ef3276d1671a0fb9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
