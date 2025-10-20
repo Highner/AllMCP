d711f617fddf24683fb8bcc51c502b4aad5be621
Highner <hvrantzau@hotmail.com>
2025-10-20 13:15:21 +0200

    Allow inventory intake without sub-appellation

--- AllMCPSolution/Models/SubAppellation.cs
+++ AllMCPSolution/Models/SubAppellation.cs
@@ -3,7 +3,7 @@ namespace AllMCPSolution.Models;
 public class SubAppellation
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
 
     public Guid AppellationId { get; set; }
     public Appellation Appellation { get; set; } = null!;
