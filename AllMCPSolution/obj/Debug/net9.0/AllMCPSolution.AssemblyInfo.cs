4a099987d5556c4f79f0184ec194e41ff4db746f
johann <johann@bowlinedigital.com>
2025-10-21 22:31:14 +0200

    sdfs

--- AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
+++ AllMCPSolution/obj/Debug/net9.0/AllMCPSolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6ffb59fab196d0942a8c6d0df13aa64c58f3fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825ab9f663802d2f5da536749c17ece9ee78763a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AllMCPSolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
